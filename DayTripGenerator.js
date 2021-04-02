7e9e88ee0a7c6502d226eb48d9cd2c922f318d90
unknown <asorenson01@hotmail.com>
2021-04-02 15:40:31 -0400

    message

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -31,65 +31,65 @@ entertainmentAlt = " ";
 //functions
 
 //Option should equal Destination, Restaurant, Transportation, or Entertainment
-function randomNumber(string1){
+function randomNumber(message){
     let x;
-    x = prompt("Press Enter to Randomly Select a " + string1);
+    x = prompt("Press Enter to Randomly Select a " + message);
     let result = Math.floor(Math.random() * 6);  
     return result;
 }
 
 
-function selectAnOption(anArray,string1){
+function selectAnOption(anArray,message){
     let result;
     let ranNum;
-    ranNum = randomNumber(string1)
+    ranNum = randomNumber(message)
     result = anArray[ranNum]
     return result;    
 }
 
 
-function pickDestination(anArray,string1){
+function pickDestination(anArray,message){
    let userInput = "No"
     console.log("Lets get ready to plan the best trip ever, I hope you are ready!");
     console.log(`Lets figure out ${destination}` );    
     while (userInput === "No"){
-        destination=selectAnOption(anArray,string1);
+        destination=selectAnOption(anArray,message);
         userInput = prompt(`Are you happy with ${destination} as your destination? Please type "Yes", or "No"`);
     }
     console.log(`So you are headed to ${destination}`);
     
 }
 
-function pickRestaurant(anArray,string1){
+function pickRestaurant(anArray,message){
     let userInput = "No"
      console.log(`I'm sure when we get to ${destination} we will be hungry!`)
      console.log(`Lets figure out ${restaurant}` )    
      while (userInput === "No"){
-         restaurant=selectAnOption(anArray,string1)
+         restaurant=selectAnOption(anArray,message);
          userInput = prompt(`Are you happy eatting at ${restaurant}? Please type "Yes", or "No"`)
      }
      console.log(`You will be eating at ${restaurant}`)
  }
 
  
-function pickTransportation(anArray,string1){
+function pickTransportation(anArray,message){
     let userInput = "No"
      console.log(`${destination} is pretty far!`);
      console.log(`Lets figure out ${transportation}`);    
      while (userInput === "No"){
-         transportation=selectAnOption(anArray,string1)
+         transportation=selectAnOption(anArray,message)
          userInput = prompt(`Are you happy with ${transportation} as your means of transportion? Please type "Yes", or "No"`);
      }
      console.log(`So you are headed to ${destination} via ${transportation}`);
      
  }
 
- function pickEntertainment(anArray,string1){
+ function pickEntertainment(anArray,message){
     let userInput = "No"
      // console.log(``)
      console.log(`${entertainment}`);    
      while (userInput === "No"){
-         entertainment=selectAnOption(anArray,string1)
+         entertainment=selectAnOption(anArray,message)
          userInput = prompt(`Do you think you will be happy with ${entertainment}? Please type "Yes", or "No"`);
      }
      console.log(`Hope you enjoy ${entertainment} before heading home!`);
